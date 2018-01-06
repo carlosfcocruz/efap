5eeefc05bb1f367784c0318c3a9c180cab1ced32
carlosfcocruz <carlosfcocruz@gmail.com>
2018-01-06 15:30:24 -0400

    Add EFAP_API and removing repositories | migrating to independent API project

--- src/server/config/init.js
+++ src/server/config/init.js
@@ -1,9 +1,6 @@
 const initRoutes = require('../routes/init');
-const initDbConnection = require('../api/repositories/init');
 
 module.exports = function (app, express) {
   app.use('/static', express.static('public'));
   initRoutes(app);
-  initDbConnection();
 };
-
--- src/server/routes/products/index.js
+++ src/server/routes/products/index.js
@@ -1,5 +1,3 @@
-const productRepository = require('../../api/repositories/product');
-
 module.exports = function (app) {
   app.route('/products')
     .get(function (req, res) {
@@ -9,6 +7,5 @@ module.exports = function (app) {
   app.route('/products/:id')
     .get(function (req, res) {
       res.send(`Product id :: ${JSON.stringify(req.params, null, 2)}`);
-      productRepository.save(req.params);
     });
 };
